f3cf56ebfb5222ce912e6c6ff5b1df7d6fc93af9
Raheel <raheel_barnawi@hotmail.com>
2017-01-27 17:56:22 -0500

    designed and implemented the Mapper class

--- BigDataAssignmet1/src/MapperFirend.java
+++ BigDataAssignmet1/src/MapperFirend.java
@@ -1,4 +1,15 @@
+import java.io.IOException;
 
-public class MapperFirend {
-
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+// design and  implement  the Mapper Class
+public class MapperFirend extends Mapper<LongWritable, Text, Text, IntWritable>{
+	
+	public void map(Object key, Text value, Context context
+			) throws IOException, InterruptedException {
+		
+		
+	}
 }
