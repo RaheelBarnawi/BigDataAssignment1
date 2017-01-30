a70b6d1417a2a7c7ce731736e820545dbc065e90
Raheel <raheel_barnawi@hotmail.com>
2017-01-29 23:03:29 -0500

    modified map methods - create friends list

--- BigDataAssignmet1/src/MapperFirend.java
+++ BigDataAssignmet1/src/MapperFirend.java
@@ -8,23 +8,40 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 // design and  implement  the Mapper Class
-public class MapperFirend extends Mapper<LongWritable, Text, Text, IntWritable>{
+public class MapperFirend extends Mapper<LongWritable, Text, IntWritable, IntWritable>{
 	
 	//private final static IntWritable one = new IntWritable(1);
 	//private Text word = new Text();
-	IntWritable friend1 = new IntWritable();
-	IntWritable friend2 = new IntWritable();
+	
 	
 	
 	public void map(Object key, Text value, Context context
 			) throws IOException, InterruptedException {
 		StringTokenizer itr = new StringTokenizer(value.toString());
-		List frinedList= new ArrayList();
+		IntWritable friend1 = new IntWritable();
+		IntWritable friend2 = new IntWritable();
+		ArrayList<Integer> frinedList= new ArrayList<>();
+		itr.nextToken();
 		while (itr.hasMoreTokens()) 
 		{
+			frinedList.add(Integer.parseInt(itr.nextToken()));
 			
 		}
 		
-		
+		for( int i=0; i<frinedList.size(); i++)
+		{
+			friend1.set(frinedList.get(i));
+			for (int j=0; j<frinedList.size(); j++)
+			{
+				if(frinedList.get(i) != frinedList.get(j))
+				{
+					friend2.set(frinedList.get(j));
+					
+					context.write(friend1, friend2);
+				}
+					
+					
+			}
+		}
 	}
 }
