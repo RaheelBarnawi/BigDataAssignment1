462b5c430da836524c8cbba9abb4da87b6d73132
Raheel <raheel_barnawi@hotmail.com>
2017-01-29 23:10:07 -0500

    changed the key/value data type

--- BigDataAssignmet1/src/ReducerFriend.java
+++ BigDataAssignmet1/src/ReducerFriend.java
@@ -1,4 +1,22 @@
+import java.io.IOException;
 
-public class ReducerFriend {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class ReducerFriend  extends Reducer <IntWritable,IntWritable,Text,IntWritable> {
+	
+	private IntWritable result = new IntWritable();
+	public void reduce(IntWritable key, Iterable<IntWritable> values,
+	Context context
+	) throws IOException, InterruptedException {
+	/*int sum = 0;
+	for (IntWritable val : values) {
+	sum += val.get();
+	}
+	result.set(sum);*/
+		
+	//context.write(key, result);
+	}
 
 }
